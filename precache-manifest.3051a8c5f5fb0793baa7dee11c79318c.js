90823f0fb8ef352d6910ad5d9b930cd97f983811
aquiladev <aquila.ua@gmail.com>
2019-01-26 18:43:15 +0000

    Automated deployment to GitHub Pages: 1f5685ffe2d76a11895f65d7ed41bb102db1c27c

---
+++ precache-manifest.3051a8c5f5fb0793baa7dee11c79318c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "368e5ecb198fce2d914a",
+    "url": "./static/js/main.368e5ecb.chunk.js"
+  },
+  {
+    "revision": "4136f6b5e82961065976",
+    "url": "./static/js/1.4136f6b5.chunk.js"
+  },
+  {
+    "revision": "368e5ecb198fce2d914a",
+    "url": "./static/css/main.a9956c0b.chunk.css"
+  },
+  {
+    "revision": "573c4f7c18f52615d4b737ecb97e06b9",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8e6a8df10e6f01d4ecccaca918c8353e.js"
+  "./precache-manifest.3051a8c5f5fb0793baa7dee11c79318c.js"
 );
 
 workbox.clientsClaim();
