3f649890bb60363f2b36a8aaa1838c6ba6dbb9da
aquiladev <aquila.ua@gmail.com>
2019-06-06 09:50:04 +0000

    Automated deployment to GitHub Pages: b6a7c85143bff75096303bb17c64c91a677c6037

---
+++ precache-manifest.9f6f4d91ecdd47b9ff2bc56a174f09f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cc748f1deb9f6def84034aa5aa1b2c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13a7f7465863f575a986",
+    "url": "./static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "01ef8b7536ba9660d6cf",
+    "url": "./static/js/2.181d9192.chunk.js"
+  },
+  {
+    "revision": "13a7f7465863f575a986",
+    "url": "./static/js/main.1d94a731.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
