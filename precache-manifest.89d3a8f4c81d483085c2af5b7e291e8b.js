d0212cc9bd4309f0b1228ef2f65c75936eb443df
aquiladev <aquila.ua@gmail.com>
2019-06-08 18:35:45 +0000

    Automated deployment to GitHub Pages: 46b8ee8cff022efd1fedb9e3714aa041be26a3c9

--- precache-manifest.89d3a8f4c81d483085c2af5b7e291e8b.js
+++ precache-manifest.89d3a8f4c81d483085c2af5b7e291e8b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "41cc748f1deb9f6def84034aa5aa1b2c",
+    "revision": "04e7000ba8787c8ce1084e6cc1d7fbd1",
     "url": "./index.html"
   },
   {
-    "revision": "13a7f7465863f575a986",
+    "revision": "522e0cc985a502a4cccc",
     "url": "./static/css/main.795991f2.chunk.css"
   },
   {
-    "revision": "01ef8b7536ba9660d6cf",
-    "url": "./static/js/2.181d9192.chunk.js"
+    "revision": "0aaa0b29c2a73d543476",
+    "url": "./static/js/2.bebbf542.chunk.js"
   },
   {
-    "revision": "13a7f7465863f575a986",
-    "url": "./static/js/main.1d94a731.chunk.js"
+    "revision": "522e0cc985a502a4cccc",
+    "url": "./static/js/main.8c0e9a08.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9f6f4d91ecdd47b9ff2bc56a174f09f6.js"
+  "./precache-manifest.89d3a8f4c81d483085c2af5b7e291e8b.js"
 );
 
 self.addEventListener('message', (event) => {
